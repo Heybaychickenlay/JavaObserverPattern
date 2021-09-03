21ec787d1ff1de646131aeac3db2293efedeea98
Heybaychickenlay <76941574+Heybaychickenlay@users.noreply.github.com>
2021-09-02 21:10:26 -0700

    Update IObserver.java

--- IObserver.java
+++ IObserver.java
@@ -1,6 +1,6 @@
 /*
 * Author: Josiah Guzman
-* Just a simple command pattern that has a receiver and supports undoable actions.
+* Just a simple observer pattern with a pseudo physics engine that allows the audio and achievements systems to work with each other along with the physics engine
 * Date Made: 3/31/2021
 */
 package ObserverPattern;
