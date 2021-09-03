5bb8a73fe1f6818a8b7a39ac01ad76b608b30964
Heybaychickenlay <76941574+Heybaychickenlay@users.noreply.github.com>
2021-09-02 21:11:08 -0700

    Update Physics.java

--- Physics.java
+++ Physics.java
@@ -1,14 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* Author: Josiah Guzman
+* Just a simple observer pattern with a pseudo physics engine that allows the audio and achievements systems to work with each other along with the physics engine
+* Date Made: 3/31/2021
+*/
 package ObserverPattern;
 import java.util.ArrayList;
-/**
- *
- * @author Josiah Guzman
- */
+
 public class Physics implements ISubject{
     ArrayList<IObserver> observers = new ArrayList();
     
